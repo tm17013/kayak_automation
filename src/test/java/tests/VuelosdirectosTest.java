de943eebb91a852ea7b6311dc663a50aaed67588
diego <diegoguadron2@gmail.com>
2024-11-26 22:55:35 -0600

    Prueba 3 vuelos directos

--- src/test/java/tests/VuelosdirectosTest.java
+++ src/test/java/tests/VuelosdirectosTest.java
@@ -30,32 +30,47 @@ public class VuelosdirectosTest {
         homePage.searchKayak();
         homePage.selectKayakLink();
 
-
+//      Crea variables para el tiempo de espera
         Duration duration = Duration.ofSeconds(10);
         WebDriverWait wait = new WebDriverWait(driver, duration);
+//      Variable para el boton del menu
         WebElement menuButton = null;
-
+//        Verifica si el menu esta desplegado u da click por si no
         menuButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("mc6t-nav-button")));
         menuButton.click();
         System.out.println("Despliega el menu");
+
+//        Dentro del try esta la prueba de dar click en el apartado de vuelos directos y si no se puede acceder acaba el test
 
         try {
             driver.findElement(By.xpath("//*[@id=\"root\"]/div/header/div/div[2]/div/div[4]/div/div[1]/div[3]/nav/ul/li[3]")).click();
             System.out.println("Da click en vuelos directos");
         } catch (Exception e) {
             System.out.println("Error durante el test de búsqueda de vuelos directos: " + e.getMessage());
+            driver.quit();
         }
+
+//        Introduce el nombre del aeropuerto y lo selecciona de la lista
 
         driver.findElement(By.xpath("//*[@id=\"main\"]/div[1]/div/div[1]/div[1]/div/div")).sendKeys("Internacional de El Salvador, San Salvador, El Salvador");
         System.out.println("Introduce el aeropuesrto");
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[4]/div/div[2]/div[2]/div/ul/li[1]/div"))).click();
         System.out.println("Selecciona el aeropuerto");
+
+//        Introduce la fecha y la selecciona de la lista emergente
+
         driver.findElement(By.xpath("//*[@id=\"main\"]/div[1]/div/div[1]/div[2]/div/div/div[2]")).click();
         System.out.println("Selecciona la fecha");
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"2024-12\"]"))).click();
         System.out.println("Selecciona diciembre");
+
+//        Da click en el boton de buscar
+
         driver.findElement(By.xpath("//*[@id=\"main\"]/div[1]/div/div[1]/div[3]/button")).click();
         System.out.println("Da click en buscar ");
+
+//        Hace un conteo de los resultados
+
         List<WebElement> elementName = driver.findElements(By.className("c2A-P"));
         System.out.println("Total de elementos encontrados:"+elementName.size()+" Vuelos directos");
         System.out.println("Test de búsqueda de vuelos directos completado exitosamente.");
